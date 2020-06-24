377cd2749c0e0051d540e67d9ce7f71ecfd959ee
awitherow <au.witherow@gmail.com>
2020-06-24 10:08:31 -0400

    add dummy about page

---
+++ src/views/About/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function About() {
+  return (
+    <View>
+      <View>
+        <Text>About the</Text>
+        <Text>Yoga Collective</Text>
+      </View>
+      <View>
+        <Text>YOGA_COLLECTIVE_MISSION</Text>
+        <Text>For Students</Text>
+        <Text>YOGA_COLLECTIVE_STUDENT_BENEFITS</Text>
+        <Text>For Teachers</Text>
+        <Text>YOGA_COLLECTIVE_TEACHER_BENEFITS</Text>
+        <Text>Support the Yoga Collective</Text>
+        <Text>
+          The only service we provide is to connect Students and Teachers so
+          that they can practice yoga. We do not take comission on classes, but
+          running our service is not cost free. Development, hosting,
+          infrastructure, marketing and more all have associated costs. We've
+          put together a few options for helping support The Yoga Collective so
+          that we can continue to provide this platform. Thank you for your
+          consideration!
+        </Text>
+      </View>
+    </View>
+  );
+}
